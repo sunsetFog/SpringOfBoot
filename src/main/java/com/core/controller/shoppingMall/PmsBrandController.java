d7b4d6efef10733af36b8e706fda34e103763f2f
sunsetFog <2693281934@qq.com>
2022-06-21 14:18:06 +0800

    商品品牌665

--- src/main/java/com/core/controller/shoppingMall/PmsBrandController.java
+++ src/main/java/com/core/controller/shoppingMall/PmsBrandController.java
@@ -1,20 +1,21 @@
 package com.core.controller.shoppingMall;
 
+import com.core.apiParams.PmsBrandAddParam;
 import com.core.common.util.PageResult;
 import com.core.common.util.ResponseData;
 import com.core.common.util.ResponseDataUtil;
 import com.core.mapper.shoppingMall.PmsBrandMapper;
 import com.core.pojo.shoppingMall.PmsBrand;
+import com.core.pojo.shoppingMall.PmsProduct;
 import com.core.pojo.shoppingMall.UmsAdmin;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -41,5 +42,36 @@ public class PmsBrandController {
         PageInfo<PmsBrand> goodsPageInfo = new PageInfo<PmsBrand>(pmsBrands);
         PageResult pageResult = PageResult.getPageResult(goodsPageInfo);
         return ResponseDataUtil.buildSuccess(pageResult);
+    }
+    @ApiOperation(value = "添加品牌")
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public ResponseData brandAdd(@RequestBody PmsBrandAddParam pmsBrandAddParam) {
+        PmsBrand pmsBrand = new PmsBrand();
+        BeanUtils.copyProperties(pmsBrandAddParam, pmsBrand);
+        //如果创建时首字母为空，取名称的第一个为首字母
+        if (StringUtils.isEmpty(pmsBrand.getFirstLetter())) {
+            pmsBrand.setFirstLetter(pmsBrand.getName().substring(0, 1));
+        }
+        int count = pmsBrandMapper.insertWay(pmsBrand);
+        if (count > 0) {
+            return ResponseDataUtil.buildSuccess(count);
+        } else {
+            return ResponseDataUtil.buildError();
+        }
+    }
+    @ApiOperation(value = "更新品牌")
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public ResponseData brandUpdate(@RequestBody PmsBrandAddParam pmsBrandAddParam) {
+        PmsBrand pmsBrand = new PmsBrand();
+        BeanUtils.copyProperties(pmsBrandAddParam, pmsBrand);
+        //如果创建时首字母为空，取名称的第一个为首字母
+        if (StringUtils.isEmpty(pmsBrand.getFirstLetter())) {
+            pmsBrand.setFirstLetter(pmsBrand.getName().substring(0, 1));
+        }
+        int count = pmsBrandMapper.updateWay(pmsBrand);
+        //更新品牌时要更新商品中的品牌名称
+        PmsProduct product = new PmsProduct();
+        product.setBrandName(pmsBrand.getName());
+        return ResponseDataUtil.buildSuccess(count);
     }
 }
--- src/main/java/com/core/mapper/shoppingMall/PmsBrandMapper.java
+++ src/main/java/com/core/mapper/shoppingMall/PmsBrandMapper.java
@@ -10,4 +10,6 @@ import java.util.List;
 @Repository
 public interface PmsBrandMapper {
     List<PmsBrand> selectWay(String keyword);
+    int insertWay(PmsBrand pmsBrand);
+    int updateWay(PmsBrand pmsBrand);
 }
